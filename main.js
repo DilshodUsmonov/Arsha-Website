1ef8348b59c556d2b0a22a473ffffc5644c16231
Dilshod Usmonov <dilshodu789@gmail.com>
2022-11-20 11:51:24 +0500

    Arsha Website

---
+++ main.js
@@ -0,0 +1,5 @@
+const elbutton=document.querySelector('.buttonn');
+const elheader=document.querySelector('header');
+elbutton.addEventListener('click', (e)=> {
+elheader.classList.toggle('scrol')
+})
